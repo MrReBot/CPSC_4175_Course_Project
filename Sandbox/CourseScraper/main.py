f7ee11a2751d0395386c6e9fb611f4e501e94986
MrReBot <mrrebot@null.net>
2022-11-07 18:38:24 -0500

    Automated season updater and updated seasons

--- Sandbox/CourseScraper/main.py
+++ Sandbox/CourseScraper/main.py
@@ -5,6 +5,7 @@ import sys
 url = "https://coursesearchapi.columbusstate.edu/v1/data/courses"
 # From https://coursesearchapi.columbusstate.edu/v1/data/terms
 seasons = {"Fall":"202208", "Spring":"202302", "Summer":"202305"}
+season_names = {"Fall":"Fa","Spring":"Sp","Summer":"Su"}
 sys.path.insert(1, '../..') # Makes it so we can access Database.py
 import Database
 db = Database.Database("../../database.txt")
@@ -36,30 +37,33 @@ def get_subject(term, subject):
     return json.loads(response.text)
     
 def main():
-    #fall = get_subject(seasons["Fall"], "CPSC")
-    #print(db.all_sections())
-    #with open("fall2022.txt","r") as f:
-    #    fall = json.loads(f.read())
-    #data = get_subject("202208","CPSC")
-    #with open("tempdata.txt","w") as f:
-    #    f.write(json.dumps(data))
-    for subject in db.all_sections():
-        season = get_subject(seasons["Summer"], subject)
-        for course in season:
-            if len(course["courses"]) == 0:
-                print(f"Skipping {course}\n\n")
+    input("This will scrape the Course Search API and update the database accordingly.\nPress Enter to Continue:")
+    count = 0
+    for season, season_id in seasons.items():
+        for subject in db.all_sections():
+            try:
+                classes = get_subject(season_id, subject)
+            except json.decoder.JSONDecodeError:
+                print(f"Can't Process {subject}")
                 continue
-            temp = course["courses"][0]
-            subject = temp["subject"]
-            id = temp["courseNumber"].strip(string.ascii_letters)
-            name = f"{subject} {id}"
-            if db.course_exist(name):
-                course = db.get_course(name)
-                course.add_season("Su")
-                print(f"Adding Spring season to {course}")
-            #course = db.get_course(course)
-            #course.add_season("Fa")
-            #print(course.format(), course.seasons)
+            for course in classes:
+                if len(course["courses"]) == 0:
+                    print(f"Skipping {course}\n\n")
+                    continue
+                temp = course["courses"][0]
+                subject = temp["subject"]
+                id = temp["courseNumber"].strip(string.ascii_letters)
+                name = f"{subject} {id}"
+                if db.course_exist(name):
+                    course = db.get_course(name)
+                    if season_names[season] not in course.get_seasons():
+                        course.add_season(season_names[season])
+                        print(f"Adding {season} season to {course}")
+                        count += 1
+                #course = db.get_course(course)
+                #course.add_season("Fa")
+                #print(course.format(), course.seasons)
+    print(f"Updated {count} entires")
     db.save()
         
         
